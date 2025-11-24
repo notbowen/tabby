75b330bdd867ad1bdd0f27ce83409403d9361104
Hu Bowen <contact@hubowen.dev>
2025-11-24 18:07:29 +0800

    add command line arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use std::error::Error;
 
 use crate::state::GameState;
@@ -7,10 +8,29 @@ mod moves;
 mod pieces;
 mod state;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Run Perft with depth n
+    #[arg(short, long)]
+    perft: Option<u8>,
+}
+
+fn run_engine() -> Result<(), Box<dyn Error>> {
+    // [TODO]: Run a UCI compatible engine from here
     let mut b = GameState::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")?;
-    // b.show_bitboards();
     b.debug_board();
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    if let Some(_n) = args.perft {
+        todo!("Implement perft with depth n");
+    } else {
+        run_engine()?;
+    }
 
     Ok(())
 }
