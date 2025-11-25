1369951dacf37a9108238c3db5f055c7c6052c3a
notbowen <contact@hubowen.dev>
2025-11-25 17:59:38 +0800

    refactor to take in FEN as arg & movegen

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use std::error::Error;
 
-use crate::state::GameState;
+use crate::{moves::movegen::MoveGen, state::GameState};
 
 mod errors;
 mod moves;
@@ -14,22 +14,41 @@ struct Args {
     /// Run Perft with depth n
     #[arg(short, long)]
     perft: Option<u8>,
+
+    /// FEN of starting position (defaults to normal)
+    #[arg(short, long)]
+    fen: Option<String>,
 }
 
-fn run_engine() -> Result<(), Box<dyn Error>> {
+fn run_engine(fen: &str) -> Result<(), Box<dyn Error>> {
     // [TODO]: Run a UCI compatible engine from here
-    let mut b = GameState::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")?;
-    b.debug_board();
+    let mut b = GameState::from_fen(fen)?;
+    b.print_board();
+    Ok(())
+}
+
+fn perft(fen: &str, _n: u8) -> Result<(), Box<dyn Error>> {
+    let mut state = GameState::from_fen(fen)?;
+    let movegen = MoveGen::new();
+
+    movegen.generate_moves(&mut state);
+
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
-    if let Some(_n) = args.perft {
-        todo!("Implement perft with depth n");
+    let fen = if let Some(f) = args.fen {
+        f
     } else {
-        run_engine()?;
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".into()
+    };
+
+    if let Some(n) = args.perft {
+        perft(&fen, n)?;
+    } else {
+        run_engine(&fen)?;
     }
 
     Ok(())
